27f04c70e7f08c75db49e4864a55c918a2f1cea8
vodaiphuoc <phuocvodn98@gmail.com>
2025-05-09 17:21:42 +0700

    new repo

---
+++ create_dataset.py
@@ -0,0 +1,87 @@
+from datasets import load_dataset
+import datasets
+# empathetic_dialogues
+data = load_dataset("facebook/empathetic_dialogues", trust_remote_code=True)
+traindata = data['train']
+testdata = data['test']
+
+
+def make_dataset(
+        data: datasets.arrow_dataset.Dataset, 
+        system_prompt:str = "You are a helpfull assistant always give emotional reponse in conservation"
+    )->datasets.arrow_dataset.Dataset:
+    r"""
+    Example of conversation data in prompt-completion format:
+    prompt_completion_example = {
+        "prompt": [{"role": "user", "content": "What color is the sky?"}],
+        "completion": [{"role": "assistant", "content": "It is blue."}]
+    }
+    """
+    
+    def _handler(row: dict):
+        history = None
+        if row['utterance_idx'] > 1:
+            history_data = data.filter(
+                lambda x: x['conv_id'] == row['conv_id'] and \
+                        x['utterance_idx'] < row['utterance_idx']
+            )
+            if len(history_data) == 0:
+                history = []
+            else:
+                history = history_data.map(
+                    lambda x: {
+                        'content': f"SpeakerID_{x['speaker_idx']}: {x['utterance']}\n" 
+                    }
+                )['content']
+            
+        else:
+            history = []
+
+        user_content = f"""
+Conversation context: {row['context']}
+Conversation history:
+{''.join(history)}
+"""
+        if len(history) > 0:
+            messages = [
+                    {
+                        "role": "system",
+                        "content": system_prompt
+                    },
+                    {
+                        "role": "user",
+                        "content": user_content
+                    },
+                    
+            ]
+        else:
+            messages = None
+        return {
+            "prompt": messages,
+            "completion": [{
+                "role": "model",
+                "content": f"{row['utterance']}"
+            }]
+        }
+
+    data = data.map(
+        lambda x: _handler(x),
+        batch_size = 5000,
+        batched = False
+    )
+    data = data.filter(lambda x: x['prompt'] is not None)
+    return data.select_columns(['prompt', 'completion'])
+
+if __name__ == '__main__':
+    import time
+    import os
+
+    _start_time = time.time()
+    datasets.disable_progress_bars()
+    converted_testdata = make_dataset(testdata)
+    converted_traindata = make_dataset(traindata)
+    print('total pre-process data time: ', time.time() - _start_time)
+
+    
+    converted_testdata.save_to_disk(os.path.join(os.path.dirname(__file__),"data","testdata"))
+    converted_traindata.save_to_disk(os.path.join(os.path.dirname(__file__),"data","traindata"))
---
+++ train.py
@@ -0,0 +1,121 @@
+
+import os
+os.environ["WANDB_DISABLED"] = "true"
+
+
+def get_model_tokenizer():
+    # from trl import (
+    #     get_kbit_device_map,
+    #     get_peft_config,
+    #     get_quantization_config,
+    # )
+    from transformers import (
+        AutoTokenizer, 
+        AutoModelForCausalLM, 
+        BitsAndBytesConfig
+    )
+    
+    model_id = "google/gemma-3-1b-it"
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    
+    # import torch
+    # bnb_config = BitsAndBytesConfig(
+    #     load_in_4bit=True,
+    #     bnb_4bit_quant_type="nf4",
+    #     bnb_4bit_compute_dtype=torch.bfloat16
+    # )
+    from accelerate import PartialState
+    device_string = PartialState().process_index
+    
+    model = AutoModelForCausalLM.from_pretrained(
+        model_id, 
+        # quantization_config=bnb_config,
+        attn_implementation='eager',
+        device_map={'':device_string}
+    )
+    return model, tokenizer
+
+def get_datasets(
+        train_path: str = "./data/traindata", 
+        test_path:str = "./data/traindata"
+    ):
+    from datasets import load_from_disk
+    return load_from_disk(train_path), load_from_disk(test_path)
+
+def main():
+    
+    model, tokenizer = get_model_tokenizer()
+
+    converted_traindata, converted_testdata = get_datasets()
+
+    import numpy as np
+    from torchmetrics.functional.text import bleu_score
+
+    def preprocess_logits_for_metrics(logits, labels):
+        if isinstance(logits, tuple):
+            logits = logits[0]
+        return logits.argmax(dim=-1)
+
+    def compute_metrics(eval_preds):
+        preds, labels = eval_preds
+    
+        # In case the model returns more than the prediction logits
+        if isinstance(preds, tuple):
+            preds = preds[0]
+    
+        preds = np.where(preds != -100, preds, tokenizer.pad_token_id)
+        labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+        
+        decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
+        decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+        
+        # Some simple post-processing
+        decoded_preds = [pred.strip() for pred in decoded_preds]
+        decoded_labels = [[label.strip()] for label in decoded_labels]
+        
+        result = bleu_score(preds=decoded_preds, target=decoded_labels)
+        return {"bleu": result}
+    
+    from trl import SFTConfig, SFTTrainer
+    from peft import LoraConfig
+    
+    lora_config = LoraConfig(
+        r=8,
+        target_modules=["q_proj", "o_proj", "k_proj", "v_proj", "gate_proj", "up_proj", "down_proj"],
+        task_type="CAUSAL_LM",
+    )
+
+    trainer = SFTTrainer(
+        model = model,
+        processing_class = tokenizer,
+        train_dataset = converted_traindata,
+        eval_dataset = converted_testdata,
+        compute_metrics = compute_metrics,
+        preprocess_logits_for_metrics = preprocess_logits_for_metrics,
+        args = SFTConfig(
+            do_train = True,
+            do_eval = False,
+            num_train_epochs = 4,
+            per_device_train_batch_size = 4,
+            per_device_eval_batch_size = 4,
+            gradient_accumulation_steps= 3,
+            warmup_steps=2,
+            completion_only_loss = True,
+            learning_rate=2e-4,
+            bf16=True,
+            bf16_full_eval = True,
+            max_length = 128,
+            max_seq_length = 128,
+            logging_strategy = 'epoch',
+            output_dir="outputs",
+            optim="paged_adamw_8bit"
+        ),
+        peft_config=lora_config, # lora config
+    )
+        
+    trainer.train()
+    trainer.save_model(os.path.join(os.path.dirname(__file__),"checkpoints"))
+    trainer.evaluate()
+
+if __name__ == '__main__':
+    main()
