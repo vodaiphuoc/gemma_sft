5a06fd1c377012f12886e1bd09caeddfc51c481a
vodaiphuoc <phuocvodn98@gmail.com>
2025-05-09 17:50:20 +0700

    correct data sample path

--- train.py
+++ train.py
@@ -37,7 +37,7 @@ def get_model_tokenizer():
 
 def get_datasets(
         train_path: str = "./data/traindata", 
-        test_path:str = "./data/traindata"
+        test_path:str = "./data/testdata"
     ):
     from datasets import load_from_disk
     return load_from_disk(train_path), load_from_disk(test_path)
@@ -46,7 +46,10 @@ def main():
     
     model, tokenizer = get_model_tokenizer()
 
-    converted_traindata, converted_testdata = get_datasets()
+    converted_traindata, converted_testdata = get_datasets(
+        train_path = os.path.dirname(__file__) + "/sample_data/traindata", 
+        test_path = os.path.dirname(__file__) + "/sample_data/testdata"
+    )
 
     import numpy as np
     from torchmetrics.functional.text import bleu_score
