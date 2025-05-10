edb584c91576292bb24e33a25b1a880a0b6543b6
vodaiphuoc <phuocvodn98@gmail.com>
2025-05-11 01:59:15 +0700

    add install pip in .sh

--- run_gemma.sh
+++ run_gemma.sh
@@ -11,9 +11,21 @@ case "$1" in
 
         if [ "${run_with_tpu}" == "true" ]; then
             echo run with tpu
+            
+            echo setup optimum-tpu for tpu
+            pip install optimum-tpu -f https://storage.googleapis.com/libtpu-releases/index.html
+            
+            echo setup dependencies in requirement
+            pip install -q -U -r rain_requirements.txt
+
+            echo run train_gemma.py
             accelerate launch --config_file config/gemma_tpu.yaml train_gemma.py
 
         elif [ "${run_with_tpu}" == "false" ]; then
+
+            echo setup dependencies in requirement
+            pip install -q -U -r train_requirements.txt
+            
             echo run with gpu
             accelerate launch --config_file config/gemma.yaml train_gemma.py
         
