995cb84cb8b6fe9f8a76d66b7df83d3ef0bf3ed0
vodaiphuoc <phuocvodn98@gmail.com>
2025-05-11 01:41:21 +0700

    remove device on tpu

--- commons/model.py
+++ commons/model.py
@@ -8,13 +8,13 @@ def get_model_tokenizer(model_id:str = "google/gemma-3-1b-it"):
     
     tokenizer = AutoTokenizer.from_pretrained(model_id)
     
-    from accelerate import PartialState
-    device_string = PartialState().process_index
+    # from accelerate import PartialState
+    # device_string = PartialState().process_index
     
     model = AutoModelForCausalLM.from_pretrained(
         model_id,
         attn_implementation='eager',
-        device_map={'':device_string}
+        # device_map={'':device_string}
     )
 
     if tokenizer.chat_template is None:
