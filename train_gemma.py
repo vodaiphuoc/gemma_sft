06026223f4b3746f9af31cd359aa8ba839d50e27
vodaiphuoc <phuocvodn98@gmail.com>
2025-05-11 17:36:02 +0700

    adjust batch size

--- train_gemma.py
+++ train_gemma.py
@@ -7,12 +7,12 @@ def main():
     training_process(
         model_id = "google/gemma-3-1b-it", 
         data_version = "2_0",
-        ratio = 0.05,
+        ratio = 0.01,
         checkpoint_save_dir = os.path.join(os.path.dirname(__file__),"checkpoints"),
         use_lora = True,
         num_train_epochs = 3,
-        train_batch_size = 10,
-        eval_batch_size = 8,
+        train_batch_size = 12,
+        eval_batch_size = 12,
         learning_rate = 1e-5
     )
 
