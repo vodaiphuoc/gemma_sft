70b30f8841c22165ce69bad422b45ffef4df52e9
vodaiphuoc <phuocvodn98@gmail.com>
2025-05-15 19:02:35 +0700

    run with mock

--- commons/training.py
+++ commons/training.py
@@ -65,7 +65,7 @@ def training_process(
             "rougeL_fmeasure": rouge_value['rougeL_fmeasure']
         }
     
-    trainer = SFTTrainer(
+    trainer = MockSFTTrainer(
         model = model,
         processing_class = tokenizer,
         train_dataset = converted_traindata,
