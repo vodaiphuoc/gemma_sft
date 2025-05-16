b2847d5290524a58553dee3c2dcad2c3c0bb5794
vodaiphuoc <phuocvodn98@gmail.com>
2025-05-16 16:38:36 +0700

    change batch size

--- run_gemma.sh
+++ run_gemma.sh
@@ -40,8 +40,8 @@ case "$1" in
                 train.py \
                 --distribution_type cuda \
                 --model_key gemma \
-                --train_batch_size 12 \
-                --eval_batch_size 12
+                --train_batch_size 8 \
+                --eval_batch_size 8
         
         else
             echo unknow value of tpu option: "${run_with_tpu}"
