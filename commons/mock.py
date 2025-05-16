e9c95a1b07f1aaa369a7054dc458fac1b2ff69a1
vodaiphuoc <phuocvodn98@gmail.com>
2025-05-16 16:55:15 +0700

    only packing for train dataset

--- commons/mock.py
+++ commons/mock.py
@@ -43,34 +43,44 @@ class MockSFTTrainer(SFTTrainer):
     def _prepare_dataset(self, *args, **kwargs)->Dataset:
         dataset = super()._prepare_dataset(*args, **kwargs)
 
-        print('check output dataset col names: ',dataset.column_names)
-        dataset = dataset.select_columns(self._COLUMN_NAMES)
-
         # get sft config
         config = None
+        dataset_name = None
         for _arg in args:
             if isinstance(_arg, SFTConfig):
                 config = _arg
+            if isinstance(_arg,str) and _arg in ['train','val']:
+                dataset_name = _arg
+
         for _, _kwarg_value in kwargs.items():
             if isinstance(_kwarg_value, SFTConfig):
                 config = _kwarg_value
+            if isinstance(_arg,str) and _arg in ['train','val']:
+                dataset_name = _arg
+        
+        if dataset_name == 'val':
+            return dataset
+        else:
+            print('running packing for train dataset')
+            print('check output dataset col names: ',dataset.column_names)
+            dataset = dataset.select_columns(self._COLUMN_NAMES)
 
-        chunk_ids = self._find_chunk_ids(
-            dataset = dataset, 
-            max_length = config.max_length
-        )
-        print('get number of chunk:', len(chunk_ids))
-        def _dataset_gen(chunk_ids: List[List[int]]):
-            for chunk in chunk_ids:
-                chunk_dataset = dataset.select(chunk)
-                yield {
-                    col: [ele for each_ids in chunk_dataset[col] for ele in each_ids]
-                    for col in self._COLUMN_NAMES
-                }
+            chunk_ids = self._find_chunk_ids(
+                dataset = dataset, 
+                max_length = config.max_length
+            )
+            print('get number of chunk:', len(chunk_ids))
+            def _dataset_gen(chunk_ids: List[List[int]]):
+                for chunk in chunk_ids:
+                    chunk_dataset = dataset.select(chunk)
+                    yield {
+                        col: [ele for each_ids in chunk_dataset[col] for ele in each_ids]
+                        for col in self._COLUMN_NAMES
+                    }
 
-        packed_dataset = Dataset.from_generator(
-            _dataset_gen, 
-            gen_kwargs = {"chunk_ids": chunk_ids}
-        )
-        print('lenght packed dataset: ', packed_dataset)
-        return packed_dataset.select_columns(self._COLUMN_NAMES)
+            packed_dataset = Dataset.from_generator(
+                _dataset_gen, 
+                gen_kwargs = {"chunk_ids": chunk_ids}
+            )
+            print('lenght packed dataset: ', packed_dataset)
+            return packed_dataset.select_columns(self._COLUMN_NAMES)
