4cad6fa231cbb8e988e7f100cc9463d88c7fe5ed
vodaiphuoc <phuocvodn98@gmail.com>
2025-05-17 23:53:42 +0700

    run with full data

--- train.py
+++ train.py
@@ -25,7 +25,7 @@ def main(
         pre_init = pre_init,
         model_key = model_key,
         data_version = "2_1",
-        ratio = 0.1,
+        ratio = None,
         distribution_device = distribution_device,
         distribution_type = distribution_type,
         checkpoint_save_dir = os.path.join(os.path.dirname(__file__),"checkpoints"),
