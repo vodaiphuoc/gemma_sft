5ef35bfa9e8f2e22f67abd690963bb17e68a356b
vodaiphuoc <phuocvodn98@gmail.com>
2025-05-18 17:39:48 +0700

    update lora2 with reducing number of trainable layers

--- commons/constants.py
+++ commons/constants.py
@@ -20,7 +20,7 @@ LORA_PARAMS2 = {
     "lora_alpha": 32,
     "lora_dropout": 0.05,
     "bias":"none",
-    "target_modules": ["all-linear"],
+    "target_modules": ["q_proj", "o_proj", "k_proj", "v_proj"],
     "task_type": "CAUSAL_LM"
 }
 
