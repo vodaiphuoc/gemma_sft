185c71d303c0e8fdf81e08b04fac44b1c4c4dea5
vodaiphuoc <phuocvodn98@gmail.com>
2025-05-19 01:53:38 +0700

    new ver data

--- create_datasetv2.py
+++ create_datasetv2.py
@@ -3,34 +3,39 @@ import datasets
 from typing import Set, List, Dict, Any, Literal
 import itertools
 from tqdm import tqdm
+import json
 
 datasets.disable_progress_bars()
 SYSTEM_PROMPT:str = "You are a helpfull assistant always give emotional reponse in conservation"
 
 def _process_history(conv_data: List[Dict[str, Any]])->Dict[str, List[Dict[str,str]]]:
     _history = [
-        f"SpeakerID_{x['speaker_idx']}: {x['utterance'].replace('_comma_',',')}\n"
-        for x in conv_data[: len(conv_data) -1]
+        {
+            "role": "user",
+            "content": f"""
+Conversation emotion: {conv_data[-1]['context']}
+Situation: {conv_data[-1]['prompt'].replace('_comma_',',')}
+{x['utterance'].replace('_comma_',',')}
+""" 
+        } if _ith%2 == 0 else \
+        {
+            "role": "model",
+            "content": f"""
+{x['utterance'].replace('_comma_',',')}
+""" 
+        }
+        for _ith, x in enumerate(conv_data[: len(conv_data) -1])
     ]
 
-    user_content = f"""
-Conversation context: {conv_data[-1]['context']}
-Context: {conv_data[-1]['prompt'].replace('_comma_',',')}
-Conversation history:
-{''.join(_history)}
-Base on context and history of above conversation, please give emotional and appropriate reponse.
-""" 
+    prompts = [{
+        "role": "system",
+        "content": SYSTEM_PROMPT
+    }]
+
+    prompts.extend(_history)
+
     return {
-            "prompt": [
-                {
-                    "role": "system",
-                    "content": SYSTEM_PROMPT
-                },
-                {
-                    "role": "user",
-                    "content": user_content
-                }
-            ],
+            "prompt": prompts,
             "completion": [{
                 "role": "model",
                 "content": f"{conv_data[-1]['utterance'].replace('_comma_',',')}"
@@ -45,19 +50,34 @@ def make_dataset(
         dataset_type:str = Literal['train','valid','test'],
         version:str = "2.0"
     )->datasets.arrow_dataset.Dataset:
-    unique_conv_id = sorted(set(data['conv_id']),key= lambda x: [ele[-1] for ele in x.split('_')])
+    unique_conv_id = sorted(
+        set(traindata['conv_id']),
+        key= lambda x: [int(ele.split(':')[-1]) for ele in x.split('_')]
+    )
     print('number unique conv id: ', len(unique_conv_id))
 
     def _conv_gen(conv_id:str):
         conversation_data = data.filter(lambda x: x['conv_id'] == conv_id).sort("utterance_idx")
-        for ele in list(itertools.accumulate(conversation_data, _accum_handler, initial=[]))[2:]:
-            yield _process_history(ele)
+        num_speakers = len(set([ele['speaker_idx'] for ele in conversation_data]))
+
+        if num_speakers == 2:
+            _select_ids = list(range(2, len(conversation_data)+1, 2))
+            accum_data = list(itertools.accumulate(conversation_data, _accum_handler, initial=[]))
+            for _id in _select_ids:
+                yield _process_history(accum_data[_id])
+    
+        else:
+            return None
 
     def _dataset_generator(unique_conv_id: Set):
-        print(len(unique_conv_id), unique_conv_id[0])
+        # print(len(unique_conv_id), unique_conv_id[0])
         for _conv_id in tqdm(unique_conv_id, total = len(unique_conv_id)):
-            for ele in _conv_gen(_conv_id):
-                yield ele
+            _gen = _conv_gen(_conv_id)
+            if _gen is not None:
+                for ele in _gen:
+                    yield ele
+            else:
+                continue
 
     import time, os
     _start_time = time.time()
@@ -67,11 +87,14 @@ def make_dataset(
         num_proc = 4
     )
 
+    # for ele in new_data:
+    #     print(json.dumps(ele, indent = 4))
+
     new_data.save_to_disk(os.path.join(os.path.dirname(__file__),"data",version,dataset_type))
     print('duration: ', time.time() - _start_time)
 
 if __name__ == '__main__':
-    VERSION = "2_1"
+    VERSION = "3_0"
     # empathetic_dialogues
     data = load_dataset("facebook/empathetic_dialogues", trust_remote_code=True)
     traindata = data['train']
