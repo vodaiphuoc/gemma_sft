a578af6af5b720d8acb3e2e8f1cec80c619abe97
vodaiphuoc <phuocvodn98@gmail.com>
2025-05-21 18:18:31 +0700

    edit path

--- eval.py
+++ eval.py
@@ -4,6 +4,8 @@ import torch
 import os
 
 current_time = "2025-05-20_22-42-32"
+ckpt_dir = os.path.join(os.path.dirname(__file__), "checkpoints", current_time)
+result_dir = os.path.join(os.path.dirname(__file__), "inference_outputs", current_time)
 
 _, _, testdataset = get_datasets(version="3_1", ratio= 0.1)
 
@@ -13,8 +15,8 @@ s = Serving(
     distribution_device = "cuda",
     distribution_type = "ddp",
     max_length = 2176,
-    checkpoint_dir = os.path.join("checkpoints", current_time),
-    result_dir = os.path.join("inference_outputs", current_time),
+    checkpoint_dir = ckpt_dir,
+    result_dir = result_dir,
     torch_compile_config = {
             "torch_compile": True,
             "torch_compile_backend": "inductor",
