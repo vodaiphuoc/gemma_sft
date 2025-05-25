cf5595ae399e0e3afcebdf9d4dda9f900502c3f8
vodaiphuoc <phuocvodn98@gmail.com>
2025-05-25 22:23:07 +0700

    monitor embedding

--- commons/bart_utils.py
+++ commons/bart_utils.py
@@ -1,6 +1,33 @@
 import torch
 import torch.nn as nn
-from transformers.models.bart.modeling_bart import BartLearnedPositionalEmbedding
+# from transformers.models.bart.modeling_bart import BartLearnedPositionalEmbedding
+
+class BartLearnedPositionalEmbedding(nn.Embedding):
+    """
+    This module learns positional embeddings up to a fixed maximum size.
+    """
+
+    def __init__(self, num_embeddings: int, embedding_dim: int):
+        # Bart is set up so that if padding_idx is specified then offset the embedding ids by 2
+        # and adjust num_embeddings appropriately. Other models don't have this hack
+        self.offset = 2
+        super().__init__(num_embeddings + self.offset, embedding_dim)
+
+    def forward(self, input_ids: torch.Tensor, past_key_values_length: int = 0, position_ids: torch.Tensor = None):
+        """`input_ids' shape is expected to be [bsz x seqlen]."""
+        print('position_ids input: ', type(position_ids), position_ids)
+        if position_ids is None:
+            bsz, seq_len = input_ids.shape[:2]
+            position_ids = torch.arange(
+                past_key_values_length, past_key_values_length + seq_len, dtype=torch.long, device=self.weight.device
+            ).expand(bsz, -1)
+        else:
+            position_ids = position_ids.unsqueeze(0)
+
+        print('position_ids input: ', type(position_ids), position_ids)
+        return super().forward(position_ids + self.offset)
+
+
 
 class GemmaLikeSpecialTokens:
 
