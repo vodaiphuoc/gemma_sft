301f32633e4263dd718242ab50f05b1038eaa4c8
vodaiphuoc <phuocvodn98@gmail.com>
2025-06-02 18:57:44 +0700

    update infer

--- app/infer.sh
+++ app/infer.sh
@@ -3,6 +3,15 @@
 OPTS=$(getopt -o "" --long hf:,ngrok: -- "$@")
 eval set -- "$OPTS"
 
+echo setup ngrok
+curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
+  | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
+  && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
+  | sudo tee /etc/apt/sources.list.d/ngrok.list \
+  && sudo apt update \
+  && sudo apt install ngrok
+echo done setup ngrok
+
 huggingface-cli login --token $2
 
 ngrok config add-authtoken $4
