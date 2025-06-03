74b0647eee6288f42ba3afaa43b0bd3b9f5b4f67
vodaiphuoc <phuocvodn98@gmail.com>
2025-06-03 14:43:13 +0700

    update infere service

---
+++ app/infer_service/run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+app_dir=$(dirname $(dirname "$(realpath "$0")"))
+lora_module_path=$app_dir/checkpoints/2025-05-30_13-40-23
+
+OPTS=$(getopt -o "" --long hf:,ngrok: -- "$@")
+eval set -- "$OPTS"
+
+setup_path=$app_dir/infer_service/setup.sh
+bash "$setup_path"
+
+
+huggingface-cli login --token $2
+
+ngrok config add-authtoken $4
+vllm serve google/gemma-3-1b-it \
+    --dtype float16 \
+    --task generate \
+    --quantization bitsandbytes \
+    --trust_remote_code \
+    --max-model-len 512 \
+    --enable-lora \
+    --lora-modules ftlora="$lora_module_path" & \
+ngrok http http://0.0.0.0:8000 
---
+++ app/infer_service/setup.sh
@@ -0,0 +1,12 @@
+echo setup ngrok
+curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
+  | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
+  && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
+  | sudo tee /etc/apt/sources.list.d/ngrok.list \
+  && sudo apt -qq update \
+  && sudo apt -qq install ngrok
+echo done setup ngrok
+
+echo setup bitsandbytes
+pip install --quiet bitsandbytes
+echo done isntall bitsandbytes
