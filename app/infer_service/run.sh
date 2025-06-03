c7f1d0d231c4371ef336f08c13201b9348e94561
vodaiphuoc <phuocvodn98@gmail.com>
2025-06-03 18:00:16 +0700

    update multi lora paths

--- app/infer_service/run.sh
+++ app/infer_service/run.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 app_dir=$(dirname $(dirname "$(realpath "$0")"))
 lora_module_path=$app_dir/checkpoints
+base_model="google/gemma-3-1b-it"
+sup_lora_path="$lora_module_path/2025-05-30_13-40-23"
+main_lora_path="$lora_module_path/2025-05-30_15-57-36"
 
-lora_modules="{
-        "name": "ftlora_sup", 
-        "path": "$lora_module_path/2025-05-30_13-40-23", 
-        "base_model_name": "google/gemma-3-1b-it"
-    } {
-        "name": "ftlora_main", 
-        "path": "$lora_module_path/2025-05-30_15-57-36", 
-        "base_model_name": "google/gemma-3-1b-it"
-    }
-"
+lora_modules='{"name": "ftlora_sup", "path": '"$sup_lora_path"',"base_model_name": '"$base_model"'}'\
+' {"name": "ftlora_main", "path": '"$main_lora_path"',"base_model_name": '"$base_model"'}'
 
+
+echo $lora_modules
 
 OPTS=$(getopt -o "" --long hf:,ngrok: -- "$@")
 eval set -- "$OPTS"
@@ -23,7 +20,7 @@ bash "$setup_path" --lora_path "$lora_module_path"
 huggingface-cli login --token $2
 
 ngrok config add-authtoken $4
-vllm serve google/gemma-3-1b-it \
+vllm serve "$base_model" \
     --dtype float16 \
     --task generate \
     --quantization bitsandbytes \
