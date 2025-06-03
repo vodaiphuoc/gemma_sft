9ac714f5bc6c799151054b922934a4f3b130eff9
vodaiphuoc <phuocvodn98@gmail.com>
2025-06-03 16:57:09 +0700

    update lora path

--- app/infer_service/run.sh
+++ app/infer_service/run.sh
@@ -19,8 +19,6 @@ vllm serve google/gemma-3-1b-it \
     --max-model-len 1024 \
     --chat-template "$lora_module_path/2025-05-30_13-40-23/chat_template.jinja"
     --enable-lora \
-    --lora-modules \
-    ftlora_sup="$lora_module_path/2025-05-30_13-40-23" \
-    ftlora_main="$lora_module_path/2025-05-30_15-57-36" \
+    --lora-modules {ftlora_sup}={"$lora_module_path/2025-05-30_13-40-23"} {ftlora_main}={"$lora_module_path/2025-05-30_15-57-36"} \
 & \
 ngrok http http://0.0.0.0:8000 
